92a895ed722b6897209d17efbcdc9ac417188c3b
Ingrid Pauchon <ingridpauchon@gmail.com>
2020-09-24 14:18:27 +0200

    completed showing the ingredients and dose description, starting on the delete button

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -20,6 +20,7 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params['id'])
+    @doses = @cocktail.doses
   end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 resources :cocktails do
   resources :doses, only: [ :new, :create ]
 end
+
+resources :doses, only: [ :destroy ]
+
 end
